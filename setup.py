0e092098f11b696548ffe20b41643e4275da8967
Ashish <ashishrao123@gmail.com>
2021-11-04 16:24:35 +0530

    "Setup and Freeze"

---
+++ setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup
+
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
+
+setup(
+    name="src",
+    version="0.0.1",
+   author="Ashishrao123",
+    description="A small package for dvc ml pipeline demo",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Ashishrao123/DVC_DL_Tensorflow_Demo_AIOps",
+    author_email="ashishrao123@gmail.com",
+    packages=["src"],
+    python_requires=">=3.7",
+    install_requires=[
+        "dvc",
+        "tensorflow",
+        "matplotlib",
+        "numpy",
+        "pandas",
+        "tqdm",
+        "PyYAML",
+        "boto3" ,
+    ]
+)
